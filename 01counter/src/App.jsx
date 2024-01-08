152392af6d842c94d8832f323aca2faa29e63aec
Rahul <rahuldevan2019@gmail.com>
2024-01-08 19:39:02 +0530

    made counter app with range 0 t0 20

--- 01counter/src/App.jsx
+++ 01counter/src/App.jsx
@@ -4,30 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let [counter,setcounter]=useState(10);
+  let addValue=()=>{
+    if(counter<21){
+      setcounter(counter+1)
+      console.log("Increment : ",counter);
+    }
+    else{
+      console.log("Out of Range");
+    }
+  }
+  let removeValue=()=>{
+    if(counter>0){
+      setcounter(counter-1);
+      console.log("Decrement ",counter);
+    }
+    else
+    console.log("-ve No. not allowed");
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Counter with Chai</h1>
+     <h2>value: {counter}</h2>
+     <button
+     onClick={addValue}
+     >Increment +1</button> 
+     <br />
+     <button
+     onClick={removeValue}
+     >Decrement -1</button>
     </>
   )
 }
