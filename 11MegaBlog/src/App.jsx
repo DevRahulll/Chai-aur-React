c98062a6c72891a5f239fe1feab078b8a47063b3
Rahul <rahuldevan2019@gmail.com>
2024-02-24 18:32:51 +0530

    appwrite section done till date

--- 11MegaBlog/src/App.jsx
+++ 11MegaBlog/src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from "react-redux"
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
-    <>
-      <h1>Hello appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logut())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, []);
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO : {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+      </div>
+  ): null
+
 }
 
 export default App
