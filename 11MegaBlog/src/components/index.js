18627750a571473a2e9b693feaf95a77cd849b98
Rahul <rahuldevan2019@gmail.com>
2024-02-26 15:22:10 +0530

    completed components folder and working with real time editor RTE, next we will move to pages

--- 11MegaBlog/src/components/index.js
+++ 11MegaBlog/src/components/index.js
@@ -3,6 +3,11 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup from './Signup'
+import Login from './Login'
+import RTE from "./RTE";
+import Select from './Select'
+import Input from './Input'
 
 
 export {
@@ -11,5 +16,11 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    Signup,
+    Login,
+    RTE,
+    Select,
+    Input,
+    
 
 }
---
+++ 11MegaBlog/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup as SignupComponent } from '../components'
+
+const Signup = () => {
+  return (
+    <div className='py-8'>Signup</div>
+  )
+}
+
+export default Signup
